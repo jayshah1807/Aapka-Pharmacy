ea94fcf9b2f738c540b36d31fc1285a51eb1c1a8
jayshah1807 <67866992+jayshah1807@users.noreply.github.com>
2021-04-10 00:26:51 +0530

    Update settings.py

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -183,6 +183,12 @@ EMAIL_HOST_PASSWORD = 'kjvbntgoyxsufmrt'
 
 DEFAULT_FROM_EMAIL = 'Aapka Pharmacy <noreply@pharmacy.com>'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # PAYTM_COMPANY_NAME = "Aapka Pharmacy"   # For representation purposes 
 # PAYTM_INDUSTRY_TYPE_ID = "Retail"     # For staging environment
 # PAYTM_CHANNEL_ID = "WEB"
